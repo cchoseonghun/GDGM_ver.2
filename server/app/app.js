edcf86b00394a4308015d6b02b8aea4330d4ecef
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2022-11-01 17:07:05 +0900

    react build file 연결 추가

--- server/app/app.js
+++ server/app/app.js
@@ -3,11 +3,20 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+const path = require('path');
 const dotenv = require('dotenv');
+
+// dotenv
 dotenv.config();
+// reading React build file
+app.use(express.static(path.join(__dirname, '../../client/build')));
+// CORS policy
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('hi');
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 })
 
 module.exports = app;
--- server/app/bin/www.js
+++ server/app/bin/www.js
@@ -5,4 +5,4 @@ const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`${PORT} 포트로 서버 실행`);
-})
+});
